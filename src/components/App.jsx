097c19aa666ca5386a412f32d606a1bb78be3ce9
ndamour1 <ndamour1@my.wctc.edu>
2026-01-26 22:12:00 -0500

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
 import Country from "./components/Country";
 import "./components/App.css";
 
 function App() {
+  const [countries, setCountries] = useState([
+    { id: 1, name: 'United States', gold: 2 },
+    { id: 2, name: 'China', gold: 3 },
+    { id: 3, name: 'France', gold: 0 },
+  ]);
+
+  function handleDelete(countryId) {
+    console.log(`delete country: ${countryId}`);
+    setCountries(countries.filter((c) => c.id !== countryId));
+  }
+  
   return (
     <div>
-      <Country />
+      {countries.map((country) => (
+        <Country key={country.id} country={country} onDelete={handleDelete} />
+      ))}
     </div>
   );
 }
