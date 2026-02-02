f3289a0d1d9abbf7765ee3f6755962fd57b705a4
ndamour1 <ndamour1@my.wctc.edu>
2026-02-02 17:41:06 -0600

    fourth commit

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -2,16 +2,10 @@ import Medal from "./components/Medal";
 import "./components/App.css";
 
 export default function Country(props) {
-  const medals = useRef([
-    { id: 1, name: "gold" },
-    { id: 2, name: "silver" },
-    { id: 3, name: "bronze" },
-  ]);
-
   return (
     <div onClick={() => props.onDelete(props.country.id)}>
       {medals.map((medal) => (
-        <p>{props.country.name} <Medal /> medals</p>
+        <Medal key={medal.name} />
       ))}
     </div>
   );
--- src/components/Medal.jsx
+++ src/components/Medal.jsx
@@ -1,5 +1,17 @@
 export default function Medals(props) {
-    return (
-    <p>{props.medals.name}</p>
+  function medalType(medal) {
+    if (medal = "gold") {
+      return <button onClick={decrementGold(props.country.id)}>-</button> + " " + getTotalMedalCount(medal) + " " + <button onClick={incrementGold(props.country.id)}>+</button>;
+    }
+    else if (medal = "silver") {
+      return <button onClick={decrementSilver(props.country.id)}>-</button> + " " + getTotalMedalCount(medal) + " " + <button onClick={incrementSilver(props.country.id)}>+</button>;
+    }
+    else if (medal = "bronze") {
+      return <button onClick={decrementBronze(props.country.id)}>-</button> + " " + getTotalMedalCount(medal) + " " + <button onClick={incrementBronze(props.country.id)}>+</button>;
+    }
+  }
+  
+  return (
+    <p>{props.medals.name} medals: {medalType(props.medals.name)}</p>
   );
 }
